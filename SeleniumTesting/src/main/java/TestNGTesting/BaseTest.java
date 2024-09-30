8f9ca7a08972eb172ea41ee3157937d0dcea1cfc
Deepti <mahatmadeepti@gmail.com>
2024-09-29 22:33:12 -0500

    commiting best

---
+++ SeleniumTesting/src/main/java/TestNGTesting/BaseTest.java
@@ -0,0 +1,22 @@
+package TestNGTesting;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseTest {
+
+	public WebDriver driver;
+	
+	public WebDriver getDriver(String browser) {
+		if (browser.equals("chrome")) {
+			//WebDriverManager.chromedriver.setup();
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\Ankit\\Downloads\\chromedriver_win32_1\\chromedriver.exe");
+			driver=new ChromeDriver();
+		}else if (browser.equals("firefox")) {
+			//WebDriverManager.firefoxdriver.setup();
+			driver=new FirefoxDriver();
+	}
+		return driver;
+}
+}
